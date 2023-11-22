dd4aded54775fe582643a285c229f645b8227f77
Sutheera P <64070112@kmitl.ac.th>
2023-11-22 22:14:07 +0700

    Update Herosection.js

--- aha_au/src/app/component/Herosection.js
+++ aha_au/src/app/component/Herosection.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Herosection = () => {
   return (
     <>
-    <div className='flex flex-col md:flex-row-reverse py-36 md:py-60 container mx-auto px-4 lg:px-36'>
-        <div className='text-center px-2 md:basis-1/2 py-16'>Logo</div>
-        <div className=''>
-            <div className='text-4xl md:text-6xl lg:text-7xl font-bold text-center md:text-left'>Apex Heroic Academy</div>
-            <div className='text-faded text-xs md:text-base text-center mt-4 md:text-left'>a scifi superhero school project</div>
+      <div className="flex h-screen">
+        <div className="m-auto">
+          <div className="flex flex-col md:flex-row-reverse container mx-auto px-4 lg:px-36">
+            <div className="text-center px-2 md:basis-1/2 py-16">Logo</div>
+            <div className="">
+              <div className="text-4xl md:text-6xl lg:text-7xl font-bold text-center md:text-left">
+                Apex Heroic Academy
+              </div>
+              <div className="text-faded text-xs md:text-base text-center mt-4 md:text-left">
+                a scifi superhero school project
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Herosection
+export default Herosection;
