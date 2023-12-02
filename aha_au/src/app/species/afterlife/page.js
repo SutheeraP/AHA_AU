2ec655449f2be3fd2e52d1cf435d1bb584464894
Sutheera P <64070112@kmitl.ac.th>
2023-12-03 00:27:13 +0700

    update afterlife

--- aha_au/src/app/species/afterlife/page.js
+++ aha_au/src/app/species/afterlife/page.js
@@ -7,7 +7,17 @@ import Footer from "@/app/component/Footer";
 export default function page() {
   const thiscontent = (
     <div>
-      
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-8 text-sm font-light">
+        <div className="col-span-2"></div>
+        <div className="col-span-3">
+          <div className="flex flex-col gap-4">
+            <div>การมีอยู่ของโลกหลังความตายเป็นที่ถกเถียงกันมานานกว่าพันปี สิ่งเหล่านี้ไม่เคยได้รับการพิสูจน์จนกระทั่งประตูมิติถูกเปิดออก คนทั้งโลกจึงได้เห็นถึงความสวยงามและความโสมมของสองสถานที่ที่เคยถูกเขียนบนตำราทางศาสนานับร้อยเล่ม ‘นรก’ และ ‘สวรรค์’</div>
+            <div>กำหนดให้นรกและสวรรค์เป็นขอบเขตย่อยของโลกหลังความตาย ไม่มีนอกมิติเหนือจากนี้ แต่สามารถคิดเผ่าพันธุ์พันธ์รวมถึงสิ่งมีชีวิตที่อาศัยอยู่ในนี้ขึ้นเองได้</div>
+            <div className="text-faded">หมายเหตุ : นิยามของนรกและสวรรค์ขอให้อยู่ในขอบเขตฝั่งตะวันตกเช่นทางกรีกโรมัน หรือ ศาสนาคริสต์</div>
+            <div></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 
