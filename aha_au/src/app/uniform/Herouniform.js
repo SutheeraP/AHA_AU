69379838af79d8cec1a724a803d9da149edf53f7
Sutheera P <64070112@kmitl.ac.th>
2023-12-06 21:39:57 +0700

    Update Herouniform.js

--- aha_au/src/app/uniform/Herouniform.js
+++ aha_au/src/app/uniform/Herouniform.js
@@ -7,11 +7,14 @@ const Herouniform = () => {
         <div className="bg-[url('../../public/aha_ask.jpg')] bg-no-repeat bg-cover  bg-center w-full h-full absolute z-0"></div>
         <div className="bg-zinc-900/70 bg-no-repeat bg-cover bg-center  w-full h-full absolute z-0"></div>
         <div className="w-full h-full absolute z-0 flex">
-            <div className="border border-faded w-48 h-48 md:w-96 md:h-96 top-1/3 left-4 md:top-1/4 md:left-36 rounded-full absolute z-0"></div>
+            <div className="mx-auto w-1/2 relative">
+                <div className="border border-faded ml-4 w-36 md:w-72 aspect-square right-0 top-1/3 md:top-1/4 rounded-full absolute z-0"></div>
+            </div>
+            
         </div>
         <div className="w-full h-full absolute z-0 flex">
           <div className="m-auto">
-            <div className="text-4xl font-semibold md:font-bold pl-12 md:pl-48 tracking-widest">UNIFORM</div>
+            <div className="text-4xl md:text-5xl font-semibold md:font-bold tracking-widest">UNIFORM</div>
           </div>
         </div>
       </div>
