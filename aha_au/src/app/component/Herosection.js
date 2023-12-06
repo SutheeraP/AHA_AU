a5f7f7dbdce8f5196115eb8f80475af51a3e08df
Sutheera P <64070112@kmitl.ac.th>
2023-12-06 23:46:47 +0700

    some update

--- aha_au/src/app/component/Herosection.js
+++ aha_au/src/app/component/Herosection.js
@@ -7,8 +7,14 @@ const Herosection = () => {
       <div className="flex h-screen">
         <div className="m-auto">
           <div className="flex flex-col md:flex-row-reverse container mx-auto px-4 lg:px-36">
-            <div className="mx-auto px-2 pb-24 md:py-16">
-              <Image src="./logo.svg" width={400} height={50} />
+            <div className="mx-auto px-2 pb-24 md:py-16 relative">
+              <Image
+                src="/greenblur.svg"
+                width={400}
+                height={50}
+                className="absolute"
+              />
+              <Image src="/logo.svg" width={400} height={50} />
             </div>
             <div className="my-auto">
               <div className="text-4xl md:text-6xl lg:text-7xl font-bold text-center md:text-left">
